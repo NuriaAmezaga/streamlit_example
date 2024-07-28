821682016866192f3aff0139c8af90b2725bdb16
Nuria Amezaga Sole <168557674+NuriaAmezaga@users.noreply.github.com>
2024-07-28 14:53:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,6 +143,8 @@ plt.axis('off')
 plt.title('Word Cloud of Diseases')
 st.pyplot(plt)
 
+# Just add it after st.sidebar:
+a = st.sidebar.radio('Choose:',[Pie Chart,Count Plot])
 
 # Sidebar options for visualization
 st.sidebar.markdown("### Symptom Analysis")
